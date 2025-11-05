b11acc2aa0ddcfff73d2940c95e8521c3bae6da8
buildyours-design <buildyours100@gmail.com>
2025-11-05 11:26:32 +0300

    Update route.js

--- app/api/imagekit/route.js
+++ app/api/imagekit/route.js
@@ -6,6 +6,7 @@ import { env } from "../../../env/index";
 const imagekit = new ImageKit({
   publicKey: env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
   privateKey: env.IMAGEKIT_PRIVATE_KEY,
+  urlEndpoint: env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT
 });
 
 export async function GET(){
