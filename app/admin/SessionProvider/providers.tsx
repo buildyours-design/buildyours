991aa61ffd82c240c4f8a17610c9da4f8c3d7ef0
buildyours-design <buildyours100@gmail.com>
2025-11-05 10:07:34 +0300

    Update providers.tsx

--- app/admin/SessionProvider/providers.tsx
+++ app/admin/SessionProvider/providers.tsx
@@ -2,9 +2,6 @@
 
 import {SessionProvider} from "next-auth/react"
 import React from "react"
-import {useSession} from "next-auth/react"
-import {redirect} from "next/navigation"
-import { auth } from "@/auth"
 
 type Props={
     children?: React.ReactNode
